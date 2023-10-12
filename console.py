c3887d5dd4b7be8b6644bce9e1d0956d631e30ca
johhnmunene <edwardsjohnny64@gmail.com>
2023-10-12 06:44:12 +0300

    Do_create correction

--- console.py
+++ console.py
@@ -2,11 +2,11 @@
 import cmd
 
 # Define a list of valid class names
-valid_classes = ["BaseModel", "SomeOtherClass", "AnotherClass"]
+valid_classes = ["YourClass1", "YourClass2"]
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
-    
+
     def do_create(self, line):
         if not line:
             print("**class name missing**")
@@ -14,24 +14,23 @@ class HBNBCommand(cmd.Cmd):
         class_name = line.strip()
         if class_name not in valid_classes:
             print("**class doesn't exist**")
-            return 
-        def do_destroy(self,line):
-            
-         new_instance = BaseModel() 
-         new_instance.save()  
-         print(new_instance.id)  
-    
+            return
+
+        new_instance = classes[class_name]()
+        new_instance.save()
+        print(new_instance.id)
+
     def emptyline(self):
-         """Do nothing """
-    pass
-    
+        """Do nothing"""
+        pass
+
     def do_EOF(self, line):
         """EOF command to exit the program"""
         return True
-    
+
     def do_quit(self, line):
         """Quit command to exit the program"""
         return True
-    
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
