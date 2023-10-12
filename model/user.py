ed3d2f696e18c49a28453c6596d41a5dee10214d
johhnmunene <edwardsjohnny64@gmail.com>
2023-10-12 16:21:27 +0300

    user

---
+++ model/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module creates the place class"""
+from models.base_model import BaseModel
+
+class user(BaseModel):
+    
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+    
