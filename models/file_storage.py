2c93891cd11a17dad4523937faa217e35dc52ab9
johhnmunene <edwardsjohnny64@gmail.com>
2023-10-13 07:15:29 +0300

    pycodestyle

--- models/file_storage.py
+++ models/file_storage.py
@@ -11,7 +11,6 @@ from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
-import os.path
 import json
 
 class FileStorage:
