e662d098baec450c724928202315b0800421e667
Kiruti01 <mikekirutic@gmail.com>
2023-10-14 13:25:54 +0300

    edit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -26,24 +26,26 @@ class FileStorage:
 
     def new(self, obj):
         """Set in __objects obj with key <obj_class_name>.id"""
-        ocname = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+        if obj is not None:
+            FileStorage.__objects[obj.id] = obj
 
     def save(self):
         """Serialize __objects to the JSON file __file_path."""
-        odict = FileStorage.__objects
-        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(objdict, f)
+    serialized_data = {}
+    for key ,obj in FileStorage .__objects.items():
+        serialized_data[key] = obj.to_dict()
+        with open(FileStorage.__file_path,'w') as file:
+            json.dump(serialized_data, file)
 
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
             with open(FileStorage.__file_path) as f:
-                objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
+                data = json.load(file)
+                for key, obj_data in data.items():
+
+                    obj = o (**obj_data)
+                    FileStorage.__objects[key] = obj
                     self.new(eval(cls_name)(**o))
         except FileNotFoundError:
-            return
+            pass
